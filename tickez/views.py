3ed14e8bb8bcdb62bef78bdbe2073b3d5616860d
Giuseppe Catillo <giuseppecatillo2003@gmail.com>
2024-05-21 21:56:34 +0200

    feat(common): add homepage

--- tickez/views.py
+++ tickez/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from sellers.models import Evento
+from django.utils import timezone
 
 def home_page(request):
-    #return HttpResponse("Hello world! Questa è la home page.")
-    return render(request, template_name="base.html")
+    object_list_sorted = sorted(Evento.objects.all(), key=lambda x: abs(x.data_ora - timezone.now()))
+    first_5_events = object_list_sorted[:5]
+
+    templ = "common/home_page.html"
+    ctx = {"title": "Tickez | Biglietti per Concerti, Festival e Teatro", 
+           "object_list": first_5_events}
+    return render(request, template_name=templ, context=ctx)
