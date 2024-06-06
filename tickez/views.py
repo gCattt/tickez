fd709b1b2f9730ac776c32d0141017e48798f4e1
Giuseppe Catillo <giuseppecatillo2003@gmail.com>
2024-06-06 16:30:09 +0200

    perf: adjust home page view

--- tickez/views.py
+++ tickez/views.py
@@ -1,13 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from products.models import Evento
-from django.utils import timezone
 
 def home_page(request):
-    object_list_sorted = sorted(Evento.objects.all(), key=lambda x: abs(x.data_ora - timezone.now()))
-    first_5_events = object_list_sorted[:5]
+    home_page_events = Evento.objects.order_by('data_ora')[:5]
 
     templ = "common/home_page.html"
-    ctx = {"title": "Tickez | Biglietti per Concerti, Festival e Teatro", 
-           "object_list": first_5_events}
+    ctx = {"object_list": home_page_events}
+
     return render(request, template_name=templ, context=ctx)
